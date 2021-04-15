42e8806055bf9404d4cd92d68b9518925dc5a885
MuhammedTaha02 <mtaha1905@yandex.com>
2021-04-15 20:21:49 +0300

    Update dot(s)

--- .config/sway/scripts.d/screenshot.sh
+++ .config/sway/scripts.d/screenshot.sh
@@ -14,4 +14,8 @@ case $1 in
 		grim -g "0,0 1920x1080" - | wl-copy
 		grim -g "0,0 1920x1080" - | swappy -f -
 	;;
+	fullsave)
+		_date=$(date +%d%m%Y_%H%M%S)
+		grim -g "0,0 1920x1080" $HOME/Pictures/grim/IMG_$_date.jpg && notify-send "The photo was saved to $HOME/Pictures/grim/ with the name IMG_$_date.jpg" || notify-send "The photo could not be saved"
+	;;
 esac
