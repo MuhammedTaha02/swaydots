ffc4d0b6a272ccb32b4641b32cacf92c0dbae006
MuhammedTaha02 <mtaha1905@yandex.com>
2021-04-15 19:40:23 +0300

    Update dot(s)

--- .config/sway/scripts.d/screenshot.sh
+++ .config/sway/scripts.d/screenshot.sh
@@ -2,9 +2,13 @@
 [[ "$(ps aux | grep -v grep | grep -io swappy)" == "swappy" ]] && kill $(ps aux | grep -v grep | grep -i swappy | awk '{print $2}')
 case $1 in
 	rec)
+		grim -g "0,0 1920x1080" /tmp/screenshot.png
+		feh -F /tmp/screenshot.png &
 		slurpout=$(slurp)
 		grim -g "$slurpout" - | wl-copy
+		killall feh
 		grim -g "$slurpout" - | swappy -f -
+		rm /tmp/screenshot.png
 	;;
 	full)
 		grim -g "0,0 1920x1080" - | wl-copy
